d67726600617187dcf2eebb7efdb74ab5cc4d91b
Kadir İD£ <89928342+Kadirinux@users.noreply.github.com>
2025-08-03 12:43:31 +0300

    Taş Kağıt Makas oyunu

---
+++ tkm.py
@@ -0,0 +1,29 @@
+import random
+import os
+
+
+while True:
+
+
+	c =input("Taş/Kağıt/Makas ?: ")
+	
+	i="taş","kağıt","makas"
+	a=list(i)
+	b=random.choice(a)
+	
+	"taş"<"kağıt"
+	"kağıt"<"makas"
+	"makas"<"taş"
+	
+	if c==b:
+		print("berabere: ", b, " = ", c)
+		
+	elif c>b:
+		print("Sen kazandın.. :", c, " > ",b)
+		
+	elif c<b:
+		print("Bilgisayar kazandı.. : ", c, " < ", b)
+		
+	else:
+		print("Error..")
+
