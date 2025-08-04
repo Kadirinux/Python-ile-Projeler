bcc962876dd345fb8dcacceaaaef7b14f7e1797d
Kadir İD£ <89928342+Kadirinux@users.noreply.github.com>
2025-08-05 01:05:02 +0300

    En Büyük/Küçük Sayıyı Bulma

---
+++ maxmin.py
@@ -0,0 +1,15 @@
+# kadirinux ★
+# En büyük/küçük sayı tespiti
+
+
+adet=int(input("Kaç adet sayı Girilecek: "))
+
+liste=[]
+sira=1
+for i in range(1, adet+1):
+	sayi=int(input("{}. sayı: ".format(sira)))
+	sira+=1
+	liste.append(sayi)
+	
+print("En büyük sayı: ", max(liste))
+print("En küçük sayı: ", min(liste))
