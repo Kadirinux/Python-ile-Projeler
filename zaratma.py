6134b301e4e0c6865b5c7ffc343f19bb994c5cd8
Kadir İD£ <89928342+Kadirinux@users.noreply.github.com>
2025-08-05 18:12:07 +0300

    Çift zar atma

---
+++ zaratma.py
@@ -0,0 +1,17 @@
+
+import random
+
+
+zar="123456"
+
+while True:
+	input("»")
+
+	zar1=random.choice(zar)
+	zar2=random.choice(zar)
+	
+	print("1. zar: ", zar1)
+	print("2. zar: ", zar2)
+	
+	if zar1==zar2=="6":
+		print("Duşeş..\n")
