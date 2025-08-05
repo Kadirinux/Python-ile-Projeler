8f3016b6e7cb850f2c38f527d72ab74f73ae85c1
Kadir İD£ <89928342+Kadirinux@users.noreply.github.com>
2025-08-05 18:12:57 +0300

    Kura Çekimi

---
+++ kuraç.py
@@ -0,0 +1,23 @@
+
+import random
+
+print("Kura Çekimi ✓\n")
+
+
+adet=int(input("Kaç kişisiniz: "))
+sira=""
+siram=0
+isimler=[]
+
+for i in range(0, adet):
+	sira+=str(i)
+	isim=input("{}. kişi: ".format(siram+1))
+	isimler.append(isim)
+	siram+=1
+	
+while True:
+	input("\n»")
+	x=random.choice(sira)
+	x=int(x)
+	
+	print("Talihli kişi: ", isimler[x])
